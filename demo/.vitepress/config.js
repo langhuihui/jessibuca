342e3e3228c98c47af85d357dcdb049c52fd3bd5
bosscheng <25022549@qq.com>
2021-12-01 21:22:17 +0800

    update vitepress

--- demo/.vitepress/config.js
+++ demo/.vitepress/config.js
@@ -2,13 +2,14 @@ module.exports = {
     title: 'Jessibuca',
     description: '一款纯H5直播流播放器',
     themeConfig: {
-        repo: "langhuihui/jessibuca",
+        repo: "langhuihui/jessibuca/tree/v3",
         docsBranch: "v2",
         sidebar: "auto",
         nav:[
             { text: 'API', link: '/api' },
+            { text: '2.X', link: 'http://jessibuca.monibuca.com/' },
         ],
-        logo: 'logo.png'
+        logo: 'logo.png',
     },
     head: [
         ['script', {src: '/jessibuca.js'}]
