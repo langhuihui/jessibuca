13856beefecfbaf5b603016fc91a23ab73ebf761
dexter <178529795@qq.com>
2022-08-10 10:19:38 +0800

    flvDemuxer消除对Connection的依赖

--- packages/demuxer/src/flv.ts
+++ packages/demuxer/src/flv.ts
@@ -1,18 +1,19 @@
-import { Connection } from "../../conn/src";
-import { ConnectionState } from "../../conn/src/types";
-import { FSM } from 'afsm';
 export interface FlvTag {
   type: number;
   data: Uint8Array;
   timestamp: number;
 }
+export interface FlvReader {
+  read<T extends number | Uint8Array>(need: T): Promise<Uint8Array>;
+}
 export class FlvDemuxer extends ReadableStream<FlvTag> {
-  constructor(public conn: Connection) {
+  header?: Uint8Array;
+  constructor(reader: FlvReader) {
     const t = new Uint8Array(15);//复用15个字节,前面4个字节是上一个tag的长度，跳过
     const tmp8 = new Uint8Array(4);
     const dv = new DataView(tmp8.buffer);
     const readTag = async (controller: ReadableStreamDefaultController<FlvTag>) => {
-      await conn.read(t);
+      await reader.read(t);
       const type = t[4];//tag类型，8是音频，9是视频，18是script
       tmp8[0] = 0;//首位置空，上一次读取可能会有残留数据
       tmp8.set(t.subarray(5, 8), 1);
@@ -23,26 +24,25 @@ export class FlvDemuxer extends ReadableStream<FlvTag> {
         tmp8[0] = t[11];//最高位
         timestamp = dv.getUint32(0);
       }
-      return conn.read(length).then(data => {
+      return reader.read(length).then(data => {
         controller.enqueue({ type, data: data.slice(), timestamp });
       }).catch(err => controller.error(err));
     };
     super({
       start: async controller => {
-        const firstRead = async () => {
-          console.time('flv');
-          await conn.read(9);//跳过flv头
-          return readTag(controller);
-        };
-        conn.once(FSM.INIT, () => controller.close());
-        conn.on(ConnectionState.RECONNECTED, firstRead);
-        return new Promise((resolve, reject) => {
-          if (conn.state !== ConnectionState.CONNECTED) {
-            conn.once(ConnectionState.CONNECTED, resolve);
-          } else {
-            resolve(null);
-          }
-        }).then(firstRead);
+        console.time('flv');
+        this.header = await reader.read(9);//跳过flv头
+        console.timeEnd('flv');
+        return readTag(controller);
+        // conn.once(FSM.INIT, () => controller.close());
+        // conn.on(ConnectionState.RECONNECTED, firstRead);
+        // return new Promise((resolve, reject) => {
+        //   if (conn.state !== ConnectionState.CONNECTED) {
+        //     conn.once(ConnectionState.CONNECTED, resolve);
+        //   } else {
+        //     resolve(null);
+        //   }
+        // }).then(firstRead);
       },
       pull: readTag
     });
